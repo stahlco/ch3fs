035c6e988946bec66848a159572a0b3bed273cc8
stahlco <c.stahl@tu-berlin.de>
2025-06-08 21:01:42 +0200

    Refactoring of main

--- main.go
+++ main.go
@@ -6,16 +6,19 @@ import (
 )
 
 func main() {
-	//
 	list, err := DiscoverAndJoinPeers()
 	if err != nil {
 		log.Fatalf("Setting up the membership in the cluster failed with Error: %v", err)
 	}
 
+	//Background routine which prints the memberlist
 	go func() {
 		for {
 			log.Printf("[INFO] Current Memberlist: %v", list.Members())
 			time.Sleep(time.Second * 20)
 		}
 	}()
+
+	// waits for goroutines to determine
+	select {}
 }
