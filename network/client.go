6812aef85208356b45928ceaca02d6d7fec14abd
cedricborchardt <cedric.borchardt@tu-berlin.de>
2025-06-10 18:13:55 +0200

    dummy client added

--- network/client.go
+++ network/client.go
@@ -1,4 +1,46 @@
 package network
 
+import (
+	commpb "ch3fs/proto"
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"time"
+)
+
 type Client struct {
+}
+
+type dummyRequest struct {
+	msg string
+}
+
+func sendDummyRequest(request string) {
+	log.Println("preparing connection...")
+
+	target := "localhost:8080" //random container
+	conn, err := grpc.NewClient(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Println("error closing the connection:", err)
+		}
+	}(conn)
+
+	client := commpb.NewFunctionsClient(conn)
+	req := &commpb.DummyReq{Msg: request}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	res, err := client.DummyTest(ctx, req)
+	if err != nil {
+		log.Println("Error from server", err)
+		return
+	}
+	log.Printf("successfully sent request, response: %s \n", res)
 }
