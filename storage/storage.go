71c68766854a6769d2b0e190bc10874f9722e8c7
stahlco <c.stahl@tu-berlin.de>
2025-06-12 11:39:56 +0200

    Impl Storage (#1)

--- storage/storage.go
+++ storage/storage.go
@@ -1,8 +1,63 @@
 package storage
 
+import (
+	"encoding/binary"
+	"encoding/json"
+	"go.etcd.io/bbolt"
+	"log"
+	"os"
+	"time"
+)
+
 type Store struct {
+	Database *bbolt.DB
 }
 
-func NewStore() *Store {
-	return nil
+type Recipe struct {
+	RecipeId int
+	filename string
+	content  string
+}
+
+func NewStore(path string, fileMode os.FileMode) *Store {
+	db, err := bbolt.Open(path, fileMode, &bbolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatalf("Opening the Database on Path: %s with FileMode: %v failed. Error: %v", path, fileMode, err)
+		return nil
+	}
+	return &Store{
+		Database: db,
+	}
+}
+
+func NewRecipe(filename string, content string) *Recipe {
+	return &Recipe{
+		filename: filename,
+		content:  content,
+	}
+}
+
+func (s *Store) StoreRecipe(recipe *Recipe) error {
+
+	return s.Database.Update(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte("recipes"))
+
+		id, _ := b.NextSequence()
+		recipe.RecipeId = int(id)
+
+		content, err := json.Marshal(recipe)
+		if err != nil {
+			log.Fatalf("Converting Recipe to JSON failed with Error: %v", err)
+			return err
+		}
+
+		return b.Put(itob(recipe.RecipeId), content)
+	})
+}
+
+// itob returns an 8-byte big endian representation of the recipe's ID
+func itob(id int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(id))
+	return b
 }
