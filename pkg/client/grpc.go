f6a60a016c450ac61615191bbd6adc0f46e2ee26
stahlco <stahl1consti2@gmail.com>
2025-07-13 23:00:13 +0200

    Added Retries on Test Client + Slides

--- pkg/client/grpc.go
+++ pkg/client/grpc.go
@@ -5,42 +5,76 @@ import (
 	"context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"log"
 	"time"
 )
 
 const ch3fTarget = "ch3f:8080"
 
 func (c *Client) UploadRecipe(target string, filename string, content []byte) (*pb.UploadResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
+	backoff := 50.0
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
 
-	conn, err := grpc.NewClient(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return nil, nil
+		conn, err := grpc.NewClient(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			return nil, nil
+		}
+
+		client := pb.NewFileSystemClient(conn)
+
+		req := &pb.RecipeUploadRequest{
+			Filename: filename,
+			Content:  content,
+		}
+		res, err := client.UploadRecipe(ctx, req)
+		if err != nil {
+			backoffBefore := backoff
+			backoff = BackoffWithJitter(backoff)
+			time.Sleep(time.Duration(backoff) * time.Millisecond)
+
+			if backoffBefore == backoff {
+				return nil, err
+			}
+			log.Printf("Now retrying with backoff: %fms", backoff)
+			continue
+		}
+
+		return res, err
 	}
-
-	client := pb.NewFileSystemClient(conn)
-
-	req := &pb.RecipeUploadRequest{
-		Filename: filename,
-		Content:  content,
-	}
-	res, err := client.UploadRecipe(ctx, req)
-	return res, err
 }
 
 func (c *Client) DownloadRecipe(filename string) (*pb.RecipeDownloadResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
+	backoff := 50.0
 
-	conn, err := grpc.NewClient(c.RoundRobin.Next(), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return nil, nil
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		conn, err := grpc.NewClient(c.RoundRobin.Next(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			return nil, err
+		}
+
+		client := pb.NewFileSystemClient(conn)
+
+		c.Logger.Infof("Sending Download Request to target: %s", conn.Target())
+		req := &pb.RecipeDownloadRequest{Filename: filename}
+
+		res, err := client.DownloadRecipe(ctx, req)
+		if err != nil {
+			backoffBefore := backoff
+			backoff = BackoffWithJitter(backoff)
+			time.Sleep(time.Duration(backoff) * time.Millisecond)
+
+			if backoffBefore == backoff {
+				return nil, err
+			}
+			log.Printf("Now retrying with backoff: %fms", backoff)
+			continue
+		}
+
+		return res, nil
 	}
-
-	client := pb.NewFileSystemClient(conn)
-
-	c.Logger.Infof("Sending Download Request to target: %s", conn.Target())
-	req := &pb.RecipeDownloadRequest{Filename: filename}
-	return client.DownloadRecipe(ctx, req)
 }
